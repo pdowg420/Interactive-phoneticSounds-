a7c7330d86887ce8f2b8afb7e14baa8d4e84cdca
pdowg420 <103688549+pdowg420@users.noreply.github.com>
2025-03-09 21:24:11 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const phoneticSounds = {
         "iɜ": "sounds/sound_iɜ.mp3",
         "ɔɜ": "sounds/sound_ɔɜ.mp3",
         "aɜ": "sounds/sound_aɜ.mp3",
-        "ju": "sounds/sound_jʊ.mp3",
+        "ju": "sounds/sound_ju.mp3",
         "wɔ": "sounds/sound_wɔ.mp3",
         "wɘ": "sounds/sound_wɘ.mp3",
                 
